50183560e9458477af5e34111abbd0e1bb97a751
XIN YI <x@x-y.life>
2017-11-12 07:43:56 +0800

    u

--- music_synchronizer.py
+++ music_synchronizer.py
@@ -1,10 +1,20 @@
-import http.server
-import socketserver
+import time
+from playsound import playsound
+from threading import Thread
 
-PORT = 8000
+THREAD_NUM = 2
+MULTI_PLAY_INTERVAL = 0.4
+ENTRY_WAITING_TIME = 5
 
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+def music_thread():
+    mp3_local_file = '/Users/yixin/IdeaProjects/Music_Synchronizer/Music/Alan_Walker_Fade.flac'
+    playsound(mp3_local_file)
+
+
+music_threads = [Thread(target=music_thread) for _ in range(THREAD_NUM)]
+
+time.sleep(ENTRY_WAITING_TIME)
+for i in range(THREAD_NUM):
+    music_threads[i].start()
+    time.sleep(MULTI_PLAY_INTERVAL)
