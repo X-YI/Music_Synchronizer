de995f90f5b8662046ad9868fec2a2bd40296b9e
XIN YI <x@x-y.life>
2017-11-12 23:23:13 +0800

    faded 现以五十重奏的形式展现！

--- music_local_synchronizer.py
+++ music_local_synchronizer.py
@@ -2,13 +2,15 @@ import time
 from playsound import playsound
 from threading import Thread
 
-TOTAL_TIME = float(1.2)
-THREAD_NUM = 50
+TOTAL_TIME = float(1.0)
+THREAD_NUM = 3
 MULTI_PLAY_INTERVAL = TOTAL_TIME / THREAD_NUM
 ENTRY_WAITING_TIME = 0
 
 
 def music_thread():
+    # mp3_local_file = './Music/dujiajiyi.flac'
+    # mp3_local_file = './Music/xiangyiweiming.mp3'
     mp3_local_file = './Music/Alan_Walker_Fade.flac'
     playsound(mp3_local_file)
 
